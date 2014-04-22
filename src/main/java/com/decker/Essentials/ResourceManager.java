23bb85bdc483aa340b5591c0a91fe67c737d375f
DeckerCHAN <DeckerCHAN@gmail.com>
2014-04-22 15:32:31 +0800

    ResourceManager done

--- src/main/java/com/decker/Essentials/ResourceManager.java
+++ src/main/java/com/decker/Essentials/ResourceManager.java
@@ -1,6 +1,33 @@
 package com.decker.Essentials;
 
+import java.io.IOException;
+import java.io.InputStream;
+
 public class ResourceManager
 {
+
+    private ResourceManager()
+    {
+
+    }
+
+    public static ResourceManager _Instance;
+
+    public static ResourceManager getInstance()
+    {
+	if (_Instance == null)
+	{
+	    _Instance = new ResourceManager();
+	}
+	return _Instance;
+    }
+
+    public byte[] getResource(String resourceName) throws IOException
+    {
+	InputStream is = this.getClass().getResourceAsStream("/" + resourceName);
+
+	return sun.misc.IOUtils.readFully(is, -1, true);
+
+    }
 
 }
