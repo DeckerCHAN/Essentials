af6459781943b63903d0d411af5f8774ffd1f924
DeckerCHAN <DeckerCHAN@gmail.com>
2014-04-23 10:05:56 +0800

    Center per-buid done

--- src/main/java/com/decker/Essentials/Center.java
+++ src/main/java/com/decker/Essentials/Center.java
@@ -1,5 +1,47 @@
 package com.decker.Essentials;
 
-public class Center {
+import java.io.IOException;
+import java.io.OutputStream;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Center implements org.sipc.se.plugin.Plugin
+{
+
+    public void getResponse(HttpServletRequest request, HttpServletResponse response)
+    {
+	String url = request.getRequestURI();
+	String target = url.substring(url.indexOf("Essentials") + "Essentials".length() + 1);
+	OutputStream stream;
+	try
+	{
+
+	    byte[] resource = ResourceManager.getInstance().getResource(target);
+	    if(resource.length==0)
+	    {
+		response.setStatus(404);
+	    }
+
+	    stream = response.getOutputStream();
+	    stream.write(resource);
+
+	} catch (IOException e)
+	{
+
+	}
+
+    }
+
+    public String getUrl()
+    {
+	return "Essentials";
+    }
+
+    public boolean onEnable()
+    {
+
+	return true;
+    }
 
 }
