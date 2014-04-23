b08f377d3a8bbdaf2db508ccfeac703bb78fd7e4
DeckerCHAN <DeckerCHAN@gmail.com>
2014-04-23 09:32:11 +0800

    Add CategoryChild.java

---
+++ src/main/java/com/decker/Essentials/openAPI/CategoryChild.java
@@ -0,0 +1,15 @@
+package com.decker.Essentials.openAPI;
+
+public class CategoryChild
+{
+    public CategoryChild(String iconName,String title,String url)
+    {
+	this.IconName=iconName;
+	this.Title=title;
+	this.Url=url;
+    }
+    
+    public String IconName;
+    public String Title;
+    public String Url;
+}
