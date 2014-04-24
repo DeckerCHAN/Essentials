a98af4f74de53f3f9325f63ba0a32385ad35f951
DeckerCHAN <DeckerCHAN@gmail.com>
2014-04-24 13:09:27 +0800

    Change static instance mode to normal mode.

--- src/main/java/com/decker/Essentials/OpenAPI/Category/Category.java
+++ src/main/java/com/decker/Essentials/OpenAPI/Category/Category.java
@@ -5,55 +5,41 @@ import java.util.LinkedList;
 
 import com.decker.Essentials.OpenAPI.User.UserType;
 
-
 public class Category
 {
     public LinkedList<CategoryChild> AnonymousCategory;
     public LinkedList<CategoryChild> StudentCategory;
     public LinkedList<CategoryChild> TeacherCategory;
     public LinkedList<CategoryChild> AdministrarorCategory;
-    
-    private Category()
+
+    public Category()
     {
-	this.AnonymousCategory=new LinkedList<CategoryChild>();
-	this.StudentCategory=new LinkedList<CategoryChild>();
-	this.TeacherCategory=new LinkedList<CategoryChild>();
-	this.AdministrarorCategory=new LinkedList<CategoryChild>();
- }    
-    
-    
-    
-    
-    private static Category _Instance;
-    
-    public static Category GetInstance()
-    {
-	Init();
-	return _Instance;
+	this.AnonymousCategory = new LinkedList<CategoryChild>();
+	this.StudentCategory = new LinkedList<CategoryChild>();
+	this.TeacherCategory = new LinkedList<CategoryChild>();
+	this.AdministrarorCategory = new LinkedList<CategoryChild>();
     }
-    
-    
-    public static void addFunctionToCategory(UserType category,String iconName,String title,String url)
+
+    public void addFunctionToCategory(UserType category, String iconName, String title, String url)
     {
-	Init();
-	CategoryChild child =new CategoryChild(iconName,title,url);
+	CategoryChild child = new CategoryChild(iconName, title, url);
 	switch (category)
 	{
-	case Anonymous:_Instance.AnonymousCategory.add(child);break;
-	case Student:_Instance.AnonymousCategory.add(child);break;
-	case Teacher:_Instance.AnonymousCategory.add(child);break;
-	case SystemAdmin:_Instance.AnonymousCategory.add(child);break;
-	
+	case Anonymous:
+	    this.AnonymousCategory.add(child);
+	    break;
+	case Student:
+	    this.AnonymousCategory.add(child);
+	    break;
+	case Teacher:
+	    this.AnonymousCategory.add(child);
+	    break;
+	case SystemAdmin:
+	    this.AnonymousCategory.add(child);
+	    break;
+
 	default:
 	    break;
-	}
-    }
-    
-    public static void Init()
-    {
-	if(_Instance==null)
-	{
-	    _Instance=new Category();
 	}
     }
 }
