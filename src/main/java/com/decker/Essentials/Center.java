ab258c00656625a648f862a752b50d2bdbade37d
DeckerCHAN <DeckerCHAN@gmail.com>
2014-04-26 10:46:51 +0800

    Change cookie type~

--- src/main/java/com/decker/Essentials/Center.java
+++ src/main/java/com/decker/Essentials/Center.java
@@ -6,9 +6,21 @@ import java.io.OutputStream;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class Center implements org.sipc.se.plugin.Plugin
+import com.decker.Essentials.Category.Category;
+import com.decker.Essentials.User.User;
+
+
+public class Center extends org.sipc.se.plugin.PluginImpl
 {
 
+    Category category;
+    public Center()
+    {
+	super();
+	this.category=Category.getInstance();
+    }
+
+    @Override
     public void getResponse(HttpServletRequest request, HttpServletResponse response)
     {
 	String url = request.getRequestURI();
@@ -16,32 +28,44 @@ public class Center implements org.sipc.se.plugin.Plugin
 	OutputStream stream;
 	try
 	{
+	    stream = response.getOutputStream();
+	    byte[] resource;
 
-	    byte[] resource = ResourceManager.getInstance().getResource(target);
-	    if(resource.length==0)
+	    if (target.matches(".*.exec"))
+	    {
+		//dynamic generate
+		User current=new User(request.getCookies());
+		resource = Command.Execute(current,request);
+	    } else
+	    {
+		//static resource
+		resource = ResourceManager.getInstance().getResource(target);
+	    }
+
+	    if (resource.length == 0)
 	    {
 		response.setStatus(404);
 	    }
 
-	    stream = response.getOutputStream();
 	    stream.write(resource);
 
 	} catch (IOException e)
 	{
 
 	}
-
+	
     }
 
+    @Override
     public String getUrl()
     {
 	return "Essentials";
     }
 
+    @Override
     public boolean onEnable()
     {
-
+	// TODO Auto-generated method stub
 	return true;
     }
-
 }
