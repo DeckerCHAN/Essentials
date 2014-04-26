87b6af6b5ff05d49d93e1680150bbd06eb4b6bd1
DeckerCHAN <DeckerCHAN@gmail.com>
2014-04-26 10:35:52 +0800

    Add command.java

---
+++ src/main/java/com/decker/Essentials/Command.java
@@ -0,0 +1,24 @@
+package com.decker.Essentials;
+
+import javax.servlet.http.HttpServletRequest;
+
+import com.decker.Essentials.Category.Category;
+import com.decker.Essentials.User.User;
+
+public class Command
+{
+    public static byte [] Execute(User sender,HttpServletRequest request)
+    {
+	switch (request.getRequestURI().substring(request.getRequestURI().lastIndexOf('/')))
+	{
+	case "Sidebar.exec":return Category.getInstance().getJsonCategory(sender.Type).getBytes();
+	    
+	    	
+
+	default:
+	    break;
+	}
+	
+	return null;
+    }
+}
