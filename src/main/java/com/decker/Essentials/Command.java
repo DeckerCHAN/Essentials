a3d3a07e04ec90ee4aeb8eb043dfc8ae858e09c6
DeckerCHAN <DeckerCHAN@gmail.com>
2014-04-27 13:52:20 +0800

    Add argument response

--- src/main/java/com/decker/Essentials/Command.java
+++ src/main/java/com/decker/Essentials/Command.java
@@ -1,13 +1,14 @@
 package com.decker.Essentials;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import com.decker.Essentials.Category.Category;
 import com.decker.Essentials.User.User;
 
 public class Command
 {
-    public static byte [] Execute(User sender,HttpServletRequest request)
+    public static byte [] Execute(User sender,HttpServletRequest request,HttpServletResponse response)
     {
 	switch (request.getRequestURI().substring(request.getRequestURI().lastIndexOf('/')))
 	{
